3d1bd5e3fa486fb3d5012f93a38a9f16d821d258
guguggagga <gaeul79_@naver.com>
2024-10-17 22:17:12 +0900

    comment: interface에서 바로 exception던지는거 연습

--- src/main/java/com/sparta/schedule_project/repository/UserRepository.java
+++ src/main/java/com/sparta/schedule_project/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.sparta.schedule_project.repository;
 
 import com.sparta.schedule_project.entity.User;
+import com.sparta.schedule_project.exception.ResponseCode;
+import com.sparta.schedule_project.exception.ResponseException;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 /**
  * 유저 엔티티를 위한 JPA 레포지토리입니다.
@@ -12,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     User findBySeq(int seq);
 
     User findByEmail(String email);
+
+//    default User findByUsername(String username) throws ResponseException {
+//        return findByName(username).orElseThrow(()-> new ResponseException(ResponseCode.USER_NOT_FOUND));
+//    }
 }
